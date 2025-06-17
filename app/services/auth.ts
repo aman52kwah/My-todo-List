7b4143fab5ae99a1c0769139df3e1d8a8c472159
aman52kwah <amankwaamichael52@gmail.com>
2025-06-17 06:36:53 +0100

    fix auth.ts

--- app/services/auth.ts
+++ app/services/auth.ts
@@ -38,16 +38,16 @@ export async function loginUser(
     params: LoginUserParams
 ):Promise<LoginUserResponse>{
 try{
-    const res = await fetch("https://localhost:5000/",{
+    const res = await fetch("https://localhost:5000/db/index",{
         method:"GET",
         headers:{"Content-Type": "application.json"},
         body:JSON.stringify(params),
     });
     const data = await res.json();
     return data;
-} catch(error){
+} catch(error : any){
     return{
-        message: error?.message || "Login failed",
+        message: error.message || "Login failed",
         isSuccessful: false,
     };
 };
@@ -59,7 +59,7 @@ export async function registerUser(
     params: RegisterUserParams
 ):Promise<RegisterUserResponse>{
     try{
-        const res = await fetch("http://localhost:5000/auth/signup", {
+        const res = await fetch("http://localhost:5000/db/index", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify(params),
